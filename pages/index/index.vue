a6a20a6c93a59f560fa91a9cbb68edf7d2548c36
ztkuaikuai <2647731903@qq.com>
2023-08-29 11:28:19 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="home">
+		{{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello，妙记！'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
